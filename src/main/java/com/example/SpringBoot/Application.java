31c95e2cf1147baf0a54fa47799f73dca7384f7d
jiang95 <lingjun.jlj@120yibao.com>
2017-09-01 18:51:13 +0800

    fix

---
+++ src/main/java/com/example/SpringBoot/Application.java
@@ -0,0 +1,19 @@
+package com.example.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+/**
+ * * Spring Boot 应用启动类
+ * */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SpringBoot/controller/SpringBootController.java
@@ -0,0 +1,19 @@
+package com.example.SpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author lingjun.jlj
+ * @create 2017-09-01
+ **/
+@Controller
+public class SpringBootController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String springboot() {
+        return "Hello Spring Boot!";
+    }
+}
