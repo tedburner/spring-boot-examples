7abb0232e7ea5766e2c50d90cd1f548145687494
jiang95 <lingjun.jlj@120yibao.com>
2017-09-22 19:31:07 +0800

    添加druid

--- src/main/java/com/example/SpringBoot/Application.java
+++ src/main/java/com/example/SpringBoot/Application.java
@@ -3,6 +3,7 @@ package com.example.SpringBoot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import javax.xml.ws.soap.MTOM;
@@ -11,7 +12,7 @@ import javax.xml.ws.soap.MTOM;
 /**
  * * Spring Boot 应用启动类
  * */
-
+@ServletComponentScan
 @SpringBootApplication
 @MapperScan("com.example.SpringBoot.persist")
 public class Application extends SpringBootServletInitializer {
