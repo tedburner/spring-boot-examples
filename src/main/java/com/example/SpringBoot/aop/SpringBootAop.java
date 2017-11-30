7a3e46f9125fed5f91e46c6e12b5890aa347e7ce
jiang95 <1103499106@qq.com>
2017-11-30 14:51:51 +0800

    url请求耗时日志打印

---
+++ src/main/java/com/example/SpringBoot/aop/SpringBootAop.java
@@ -0,0 +1,11 @@
+package com.example.SpringBoot.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @author lingjun.jlj
+ * @create 2017-11-30
+ **/
+@Aspect
+public class SpringBootAop {
+}
