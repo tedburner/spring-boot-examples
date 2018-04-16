9c7280b111ee82b49ed07b077131875305dea918
jiang95 <1103499106@qq.com>
2018-04-16 11:30:05 +0800

    fix

--- src/test/java/com/example/SpringBoot/MainTest.java
+++ src/test/java/com/example/SpringBoot/MainTest.java
@@ -1,6 +1,6 @@
 package com.example.SpringBoot;
 
-import com.example.SpringBoot.dto.DO.CityDO;
+import com.example.SpringBoot.dto.DTO.SimpleDTO;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,10 +17,9 @@ public class MainTest {
     }
 
     public static void main(String[] args) {
-        CityDO builder = CityDO.CityBuilder.aCity()
-                .withProvinceId(1L)
+        SimpleDTO builder = SimpleDTO.SimpleDTOBuilder.aSimpleDTO()
+                .withId(1)
                 .withName("金华市")
-                .withDescription("金华火腿")
                 .build();
         System.out.println(builder);
     }
