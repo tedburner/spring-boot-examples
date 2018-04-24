55c5b3f0949edfbe8663dc68a9ba2a77888338e3
jiang95 <1103499106@qq.com>
2018-04-24 16:12:53 +0800

    fix

--- src/main/java/com/example/SpringBoot/controller/DockerController.java
+++ src/main/java/com/example/SpringBoot/controller/DockerController.java
@@ -1,9 +1,18 @@
 package com.example.SpringBoot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author lingjun.jlj
  * @data 2018/4/24
  * @Description:
  */
+@RestController
 public class DockerController {
+
+    @RequestMapping("/docker")
+    public String docker(){
+        return "Hello Docker!";
+    }
 }
