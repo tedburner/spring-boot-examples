f12fdc5bbef0ffd78a2d17b84de72962571e4b48
jiang95 <1103499106@qq.com>
2018-04-28 11:15:15 +0800

    fix

--- src/main/java/com/example/SpringBoot/aop/ServiceAop.java
+++ src/main/java/com/example/SpringBoot/aop/ServiceAop.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
  **/
 @Aspect
 @Component
-public class SpringBootAop {
+public class ServiceAop {
 
     @Before("execution(* com.example.SpringBoot.service..*.*(..))")
     public void before(){
