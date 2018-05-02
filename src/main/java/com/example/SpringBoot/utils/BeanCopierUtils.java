e39f87f05225f830d5ce7f6a38a2c4033bb42e08
jiang95 <1103499106@qq.com>
2018-05-02 16:51:15 +0800

    添加BeanCopier工具类

---
+++ src/main/java/com/example/SpringBoot/utils/BeanCopierUtils.java
@@ -0,0 +1,49 @@
+package com.example.SpringBoot.utils;
+
+import com.example.SpringBoot.model.DO.UserDO;
+import org.springframework.cglib.beans.BeanCopier;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author lingjun.jlj
+ * @data 2018/5/2
+ * @Description:
+ */
+public class BeanCopierUtils {
+
+    public static Map<String, BeanCopier> beanCopierMap = new ConcurrentHashMap<>();
+
+
+    public static void copyProperties(Object source,
+                                        Object target){
+         String beanKey = generateKey(source.getClass(),target.getClass());
+         BeanCopier copier = null;
+         if (!beanCopierMap.containsKey(beanKey)) {
+             copier = BeanCopier.create(source.getClass(), target.getClass(), false);
+             beanCopierMap.put(beanKey, copier);
+         }else {
+             copier = beanCopierMap.get(beanKey);
+         }
+         copier.copy(source, target, null);
+     }
+
+    private static String generateKey(Class<?>class1,Class<?>class2){
+        return class1.toString() + class2.toString();
+    }
+
+    public static void main(String[] args) {
+        UserDO object = UserDO.UserDOBuilder
+                .anUserDO()
+                .withId(1)
+                .withAge(20)
+                .withName("蒋")
+                .build();
+        UserDO userDO = new UserDO();
+        copyProperties(object,userDO);
+        System.out.println(userDO.getAge());
+    }
+}
