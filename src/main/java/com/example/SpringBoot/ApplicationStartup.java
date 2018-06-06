4eff6e1c27b37f7482ef6f4ba0e207ad7b709944
jiang95 <1103499106@qq.com>
2018-06-06 16:51:29 +0800

    fix

--- src/main/java/com/example/SpringBoot/ApplicationStartup.java
+++ src/main/java/com/example/SpringBoot/ApplicationStartup.java
@@ -1,9 +1,21 @@
 package com.example.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 /**
  * @author 蒋灵俊
  * @data 2018/6/6
  * @Description:
  */
-public class ApplicationStartup {
+@ServletComponentScan
+@SpringBootApplication
+@MapperScan("com.example.springboot.persist")
+public class ApplicationStartup extends SpringBootServletInitializer{
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartup.class, args);
+    }
 }
