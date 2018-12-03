648a19c46068c85f3a0aeb561a6a7a05eedf6693
Lucifer <jlj_vip98@qq.com>
2018-12-03 16:28:39 +0800

    fix docker

---
+++ spring-boot-docker/src/main/java/com/example/docker/SpringBootDockerApplication.java
@@ -0,0 +1,16 @@
+package com.example.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Lucifer
+ * docker 容器化
+ */
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+}
