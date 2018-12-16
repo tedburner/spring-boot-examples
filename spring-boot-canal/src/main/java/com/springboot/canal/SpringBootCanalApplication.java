d1f01f0f67b309daa40f42d171fa3251ef0367d3
Lucifer <jlj_vip98@qq.com>
2018-12-16 17:27:13 +0800

    canal 阿里数据库日志订阅

---
+++ spring-boot-canal/src/main/java/com/springboot/canal/SpringBootCanalApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Lucifer
+ * @desciption: canal 订阅
+ * */
+@SpringBootApplication
+public class SpringBootCanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCanalApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-canal/src/test/java/com/springboot/canal/SpringBootCanalApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springboot.canal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCanalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
