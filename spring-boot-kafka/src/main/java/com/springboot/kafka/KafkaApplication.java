53567935b5b462f1af59bb74c43603492d50421f
Arthas <jlj_vip98@qq.com>
2019-01-10 10:04:51 +0800

    fix kafka

---
+++ spring-boot-kafka/src/main/java/com/springboot/kafka/KafkaApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
+
