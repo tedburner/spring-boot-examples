18f3158030f51112685ccee8df017904ebbb6c98
arthas <jlj_vip98@qq.com>
2019-01-17 11:32:23 +0800

    fix author

--- spring-boot-docker/src/main/java/com/example/docker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/example/docker/SpringBootDockerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author : Lucifer
+ * @author : Arthas
  * docker 容器化
  */
 @SpringBootApplication
