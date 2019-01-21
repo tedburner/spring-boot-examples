4ef73f1eb3eed1e609d6f1a26d628abe233bdf6c
arthas <jlj_vip98@qq.com>
2019-01-21 16:38:49 +0800

    fix

--- spring-boot-sharding-sphere/src/main/java/com/springboot/shardingsphere/config/ShardingConfig.java
+++ spring-boot-sharding-sphere/src/main/java/com/springboot/shardingsphere/config/ShardingConfig.java
@@ -1,9 +1,11 @@
 package com.springboot.shardingsphere.config;
 
+import com.google.common.collect.Range;
 import io.shardingsphere.api.algorithm.sharding.PreciseShardingValue;
 import io.shardingsphere.api.algorithm.sharding.RangeShardingValue;
 import io.shardingsphere.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
 import io.shardingsphere.api.algorithm.sharding.standard.RangeShardingAlgorithm;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -13,65 +15,43 @@ import java.util.HashSet;
  * @date: 2018-12-28 11:50
  * @description:
  */
-public class ShardingConfig implements PreciseShardingAlgorithm, RangeShardingAlgorithm {
+@Configuration
+public class ShardingConfig implements PreciseShardingAlgorithm<Integer>, RangeShardingAlgorithm<Long> {
 
     /**
      * 精确分片算法
-     * 将user_id的值和5比较，如果小于等于5，数据对应的表为：USER_AUTH_1
-     * 将user_id的值和5比较，如果大于5，数据对应的表为：USER_AUTH_2
      *
      * @param availableTargetNames
      * @param shardingValue
      * @return
      */
     @Override
-    public String doSharding(Collection availableTargetNames, PreciseShardingValue shardingValue) {
-        if (availableTargetNames.contains("user_auth")) {
-            if (shardingValue.getColumnName().equalsIgnoreCase("USER_ID")) {
-                Comparable value = shardingValue.getValue();
-                int i = value.compareTo(5);
-                int suffix = -1;
-                if (i <= 0) {
-                    suffix = 1;
-                } else {
-                    suffix = 2;
-                }
-                String s = ("user_auth_" + suffix).toUpperCase();
-                return s;
+    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Integer> shardingValue) {
+        int size = availableTargetNames.size();
+        for (String each : availableTargetNames) {
+            if (each.startsWith(shardingValue.getValue() % size + "")) {
+                return each;
             }
         }
         return null;
     }
 
     /**
-     * lower 为 between and 中的较小值
-     * upper 为 between and 中的较大值
-     * 如果lower小于等于5，tables集合中就要加入USER_AUTH_1表
-     * 如果lower大于5，tables集合中就要加入USER_AUTH_2表
+     * 分表算法
      *
      * @param availableTargetNames
-     * @param shardingValue
+     * @param rangeShardingValue
      * @return
      */
     @Override
-    public Collection<String> doSharding(Collection availableTargetNames, RangeShardingValue shardingValue) {
-
+    public Collection<String> doSharding(Collection<String> availableTargetNames, RangeShardingValue<Long> rangeShardingValue) {
+        int size = availableTargetNames.size();
         Collection<String> tables = new HashSet<>();
-        if (availableTargetNames.contains("user_auth")) {
-            if (shardingValue.getColumnName().equalsIgnoreCase("USER_ID")) {
-                Comparable lower = shardingValue.getValueRange().lowerEndpoint();
-                Comparable upper = shardingValue.getValueRange().upperEndpoint();
-
-                int lowerResult = lower.compareTo(5);
-                int upperResult = upper.compareTo(5);
-
-                if (lowerResult <= 0) {
-                    tables.add("user_auth_1".toUpperCase());
-                    if (upperResult > 0) {
-                        tables.add("user_auth_2".toUpperCase());
-                    }
-                } else {
-                    tables.add("user_auth_2".toUpperCase());
+        Range<Long> valueRange = rangeShardingValue.getValueRange();
+        for (Long i = valueRange.lowerEndpoint(); i <= valueRange.upperEndpoint(); i++) {
+            for (String each : availableTargetNames) {
+                if (each.endsWith(i % size + "")) {
+                    tables.add(each);
                 }
             }
         }
---
+++ spring-boot-sharding-sphere/src/main/java/com/springboot/shardingsphere/config/ShardingDataSourceConfig.java
@@ -0,0 +1,16 @@
+package com.springboot.shardingsphere.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Arthas
+ * @date: 2018-12-28 11:37
+ * @description: 分片设置
+ */
+@Slf4j
+@Configuration
+public class ShardingDataSourceConfig {
+
+}
