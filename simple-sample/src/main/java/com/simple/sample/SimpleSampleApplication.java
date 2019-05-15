e94b4db5d7fb101038d2e23cd8965ab310d0ff97
jianglingjun <jianglingjun@yuuwei.cn>
2019-05-15 14:35:00 +0800

    spring使用构造注入，在启动的时候就会暴露问题

---
+++ simple-sample/src/main/java/com/simple/sample/SimpleSampleApplication.java
@@ -0,0 +1,13 @@
+package com.simple.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSampleApplication.class, args);
+    }
+
+}
