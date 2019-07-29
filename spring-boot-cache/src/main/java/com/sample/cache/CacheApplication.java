3b4b174f6e56fb58a2aaef86218fca66a8578180
tedburner <jlj_vip98@qq.com>
2019-07-29 09:46:16 +0800

    把缓存模块集成一个模块

---
+++ spring-boot-cache/src/main/java/com/sample/cache/CacheApplication.java
@@ -0,0 +1,19 @@
+package com.sample.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * @author: Arthas
+ * @description: 关于各种缓存的集成
+ */
+@EnableCaching
+@SpringBootApplication
+public class CacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class, args);
+    }
+
+}
---
+++ spring-boot-flink/src/main/java/com/sample/flink/FlinkApplication.java
@@ -0,0 +1,13 @@
+package com.sample.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkApplication.class, args);
+    }
+
+}
