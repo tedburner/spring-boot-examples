675feb3d8f3f0806f8f7e7ca4bf3d83f908859af
tedburner <jlj_vip98@qq.com>
2019-08-02 16:43:34 +0800

    修改数据源，去掉Jackson包

--- spring-boot-sample/src/main/java/com/sample/springboot/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/sample/springboot/SpringBootSampleApplication.java
@@ -13,13 +13,15 @@ import org.springframework.kafka.annotation.EnableKafka;
 /**
  * @author : Lucifer
  * @description: 启动类
+ * <p>
+ * 使用@ServletComponentScan 注解后，Servlet、Filter、Listener 可以直接通过 @WebServlet、@WebFilter、@WebListener 注解自动注册，无需其他代码
  */
 @EnableCaching
-@EnableKafka
+//@EnableKafka
 @ServletComponentScan
 @SpringBootApplication
 @MapperScan("com.sample.springboot.persist")
-@ComponentScan(basePackages = {"com.kit.common", "com.sample"})
+@ComponentScan(basePackages = {"com.kit.common", "com.sample.springboot"})
 @ImportResource("classpath:redis/spring-redis.xml")
 public class SpringBootSampleApplication {
 
--- spring-boot-sample/src/main/java/com/sample/springboot/config/DruidDBConfig.java
+++ spring-boot-sample/src/main/java/com/sample/springboot/config/DruidDBConfig.java
@@ -77,8 +77,10 @@ public class DruidDBConfig {
     @Value("{spring.datasource.connectionProperties}")
     private String connectionProperties;
 
-    //声明其为Bean实例
-    //在同样的DataSource中，首先使用被标注的DataSource
+    /**
+     * 声明其为Bean实例
+     * 在同样的DataSource中，首先使用被标注的DataSource
+     */
     @Bean
     @Primary
     public DataSource dataSource() {
