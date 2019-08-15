4b7be0227ec979680c584b64888455f224a4d072
tedburner <jlj_vip98@qq.com>
2019-08-15 11:36:09 +0800

    全局异常捕获测试，捕获中是按照顺序执行的

--- simple-sample/src/main/java/com/simple/sample/handler/GlobalExceptionHandler.java
+++ simple-sample/src/main/java/com/simple/sample/handler/GlobalExceptionHandler.java
@@ -1,18 +1,13 @@
 package com.simple.sample.handler;
 
 import com.simple.sample.exception.BaseException;
-import com.simple.sample.exception.ParamException;
 import com.simple.sample.util.http.NewResponseModel;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.map.HashedMap;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.util.logging.Logger;
 
 /**
  * @author: lingjun.jlj
@@ -28,12 +23,7 @@ public class GlobalExceptionHandler {
     public Object baseErrorHandler(BaseException exception, HttpServletRequest request, HttpServletResponse response) {
         String url = request.getRequestURL().toString();
         log.error("请求：[{}],发生自定义【BaseException】异常：[{}]-[{}]", url, exception.getCode(), exception.getMessage());
-        if (exception instanceof ParamException) {
-            ParamException paramException = (ParamException) exception;
-            return new NewResponseModel(paramException.getCode(), paramException.getMessage());
-        }else {
-            return NewResponseModel.Fail();
-        }
+        return new NewResponseModel(exception.getCode(), exception.getMessage());
     }
 
     @ExceptionHandler(value = Exception.class)
