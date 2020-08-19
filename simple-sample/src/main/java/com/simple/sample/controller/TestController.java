e544ad61d08948d7ab97c2f361befa0cc7c5c704
lingjun.jlj <jlj_vip98@qq.com>
2020-08-19 14:02:48 +0800

    bean 的继承

--- simple-sample/src/main/java/com/simple/sample/controller/TestController.java
+++ simple-sample/src/main/java/com/simple/sample/controller/TestController.java
@@ -2,22 +2,29 @@ package com.simple.sample.controller;
 
 import com.simple.sample.domain.dto.UserDTO;
 import com.simple.sample.service.common.TestService;
+import com.simple.sample.service.say.SayService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @author: lingjun.jlj
  * @date: 2019/11/27 10:27
  * @description:
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
 
     private final TestService testService;
+    @Autowired
+    private List<SayService> sayServiceList;
 
     @Autowired
     public TestController(TestService testService) {
@@ -51,5 +58,11 @@ public class TestController {
     public String synchronizedTest(@PathVariable(value = "name") String name) {
         testService.testSynchronizedChar(name);
         return "成功";
+    }
+
+    @GetMapping(value = "test")
+    public void test() {
+        log.info("====================");
+        sayServiceList.forEach(SayService::say);
     }
 }
---
+++ simple-sample/src/main/java/com/simple/sample/service/say/SayHello.java
@@ -0,0 +1,24 @@
+package com.simple.sample.service.say;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2020/8/19 12:01
+ * @description:
+ */
+@Slf4j
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class SayHello extends SayService {
+
+    @Override
+    public void say() {
+        super.say();
+        log.info("Hello");
+    }
+}
