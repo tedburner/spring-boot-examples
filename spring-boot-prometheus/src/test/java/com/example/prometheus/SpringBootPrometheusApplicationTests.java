d9df0f42ae5c271b1ed7106d67b69ef71101ea53
lingjun.jlj <jlj_vip98@qq.com>
2021-02-01 18:07:57 +0800

    :heavy_plus_sign: prometheus 监控

---
+++ spring-boot-prometheus/src/test/java/com/example/prometheus/SpringBootPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
