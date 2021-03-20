269cba2254cede6ff5a27fa9bc7028193a01db9a
tedburner <jlj_vip98@qq.com>
2021-03-20 22:05:47 +0800

    创建spring boot stater应用

---
+++ spring-boot-sample-starter/src/main/java/com/springboot/sample/starter/SampleStarterApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sample.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleStarterApplication.class, args);
+    }
+
+}
