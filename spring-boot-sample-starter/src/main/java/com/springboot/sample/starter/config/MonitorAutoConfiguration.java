0817a9e0e8e6a98dee0e9d1207e317d32787edb9
tedburner <jlj_vip98@qq.com>
2021-04-10 20:11:47 +0800

    fix

--- spring-boot-sample-starter/src/main/java/com/springboot/sample/starter/config/MonitorAutoConfiguration.java
+++ spring-boot-sample-starter/src/main/java/com/springboot/sample/starter/config/MonitorAutoConfiguration.java
@@ -5,7 +5,6 @@ import com.springboot.sample.starter.service.MonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
