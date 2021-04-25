4dd591061f99d257f71ed475c192235abd182657
lingjun.jlj <jlj_vip98@qq.com>
2021-04-25 10:34:53 +0800

    delete module

---
+++ spring-boot-cqrs/src/main/java/com/sb/cqrs/service/config/AxonConfig.java
@@ -0,0 +1,12 @@
+package com.sb.cqrs.service.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2021/4/25 10:30
+ * @description:
+ */
+@Configurable
+public class AxonConfig {
+}
