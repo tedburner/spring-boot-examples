c17200572b4986754260e5da774f000265a466ce
lingjun.jlj <jlj_vip98@qq.com>
2021-04-30 15:30:34 +0800

    SmartLifecycle

---
+++ simple-sample/src/main/java/com/simple/sample/config/TestSmartLifecycle.java
@@ -0,0 +1,72 @@
+package com.simple.sample.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.SmartLifecycle;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2021/4/30 11:21
+ * @description: SmartLifecycle测试
+ */
+@Slf4j
+@Component
+public class TestSmartLifecycle implements SmartLifecycle {
+
+    private boolean isRunning = false;
+
+    /**
+     * 1.在启动任务或者其他异步服务，比如开启MQ接收消息
+     * 2、当上下文被刷新时，调用该方法，默认生命周期处理器将检查每个SmartLifecycle对象的isAutoStartup()方法返回的布尔值。
+     * 如果为“true”，则该方法会被调用，而不是等待显式调用自己的start()方法。
+     */
+    @Override
+    public void start() {
+        log.info("start");
+        isRunning = true;
+    }
+
+    /**
+     * 接口Lifecycle的子类的方法，只有非SmartLifecycle的子类才会执行该方法。<br/>
+     * 1. 该方法只对直接实现接口Lifecycle的类才起作用，对实现SmartLifecycle接口的类无效。<br/>
+     * 2. 方法stop()和方法stop(Runnable callback)的区别只在于，后者是SmartLifecycle子类的专属。
+     */
+    @Override
+    public void stop() {
+    }
+
+    /**
+     * 1. 只有该方法返回false时，start方法才会被执行。<br/>
+     * 2. 只有该方法返回true时，stop(Runnable callback)或stop()方法才会被执行。
+     */
+    @Override
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    /**
+     * 根据该方法的返回值决定是否执行start方法。<br/>
+     * 返回true时start方法会被自动执行，返回false则不会。
+     */
+    @Override
+    public boolean isAutoStartup() {
+        return false;
+    }
+
+    /**
+     * SmartLifecycle子类的才有的方法，当isRunning方法返回true时，该方法才会被调用。
+     */
+    @Override
+    public void stop(Runnable callback) {
+
+    }
+
+    /**
+     * 如果工程中有多个实现接口SmartLifecycle的类，则这些类的start的执行顺序按getPhase方法返回值从小到大执行。<br/>
+     * 例如：1比2先执行，-1比0先执行。 stop方法的执行顺序则相反，getPhase返回值较大类的stop方法先被调用，小的后被调用。
+     */
+    @Override
+    public int getPhase() {
+        return 0;
+    }
+}
