1a18cb18089d8b329b4e6f3dccd7f7a6b20aa629
tedburner <jlj_vip98@qq.com>
2021-09-25 13:46:42 +0800

    测试线程阻塞问题的解决方案

--- spring-boot-job/src/main/java/com/springboot/job/schedule/ScheduleJob.java
+++ spring-boot-job/src/main/java/com/springboot/job/schedule/ScheduleJob.java
@@ -6,6 +6,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author: lingjun.jlj
@@ -16,8 +18,60 @@ import java.util.Date;
 @Component
 public class ScheduleJob {
 
+    private static ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
     @Scheduled(cron = "0/5 * * * * ?")
     public void task() {
         log.info("Spring Schedule execute tasK:{}", DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+    }
+
+    @Scheduled(cron = "0 0/2 * * * ?")
+    public void threadPoolTask() {
+        log.info("线程池开始执行任务");
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        AtomicInteger number = new AtomicInteger(0);
+        Semaphore semaphore = new Semaphore(10);
+        CountDownLatch latch = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                try {
+                    number.incrementAndGet();
+                    try {
+                        semaphore.acquire();
+                        if (number.get() == 1) {
+                            Thread.sleep(600000);
+                        } else {
+                            Thread.sleep(50000);
+                        }
+                        log.info("do something ... ");
+                    } catch (Exception e) {
+                        log.info("do something ... ");
+                    } finally {
+                        semaphore.release();
+                    }
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        try {
+            latch.await(1, TimeUnit.MINUTES);
+            executor.shutdownNow();
+        } catch (Exception e) {
+
+        }
+        log.info("任务执行结束");
+    }
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void threadPoolMonitor() {
+        System.out.println("=====================================thread-pool-info: =====================================");
+        System.out.println("CorePoolSize:" + executor.getCorePoolSize());
+        System.out.println("MaxPoolSize: " + executor.getMaximumPoolSize());
+        System.out.println("PoolSize:" + executor.getPoolSize());
+        System.out.println("ActiveCount:" + executor.getActiveCount());
+        System.out.println("KeepAliveTime:" + executor.getKeepAliveTime(TimeUnit.SECONDS));
+        System.out.println("QueueSize:" + executor.getQueue().size());
     }
 }
