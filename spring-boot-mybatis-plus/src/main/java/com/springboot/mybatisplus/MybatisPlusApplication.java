94ec7a7ba5414d5512e2aa6d3757331b0a65e5a5
tedburner <jlj_vip98@qq.com>
2022-03-03 22:36:48 +0800

    添加mybatis-plus使用方法

---
+++ spring-boot-mybatis-plus/src/main/java/com/springboot/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.mybatisplus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2022/3/3 21:49
+ * @description: mybatis-plus 启动类
+ */
+@SpringBootApplication
+@MapperScan("com.springboot.mybatisplus.mapper")
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
