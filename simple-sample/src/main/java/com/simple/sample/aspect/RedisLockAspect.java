c74b541f9bc73b78d7690bc295ef88952d55ac57
tedburner <jlj_vip98@qq.com>
2023-08-02 22:43:54 +0800

    fix bug

--- simple-sample/src/main/java/com/simple/sample/aspect/RedisLockAspect.java
+++ simple-sample/src/main/java/com/simple/sample/aspect/RedisLockAspect.java
@@ -18,8 +18,6 @@ import org.springframework.expression.Expression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
