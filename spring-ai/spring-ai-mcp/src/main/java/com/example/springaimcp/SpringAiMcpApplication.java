0c6587fe486cc658d5559e4db304f19f403f84b3
tedburner <jlj_vip98@qq.com>
2025-04-19 10:13:17 +0800

    新增mcp相关框架

---
+++ spring-ai/spring-ai-mcp/src/main/java/com/example/springaimcp/SpringAiMcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaimcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMcpApplication.class, args);
+    }
+
+}
---
+++ spring-ai/spring-ai-mcp/src/test/java/com/example/springaimcp/SpringAiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaimcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
