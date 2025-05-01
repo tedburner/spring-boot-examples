e2f60082222265d52d8a720f721ad3ba0b8be8c5
tedburner <jlj_vip98@qq.com>
2025-05-01 17:03:00 +0800

    添加大模型RAG模块服务

---
+++ spring-ai/spring-ai-vector/src/main/java/com/knowledge/vector/SpringAiVectorApplication.java
@@ -0,0 +1,19 @@
+package com.knowledge.vector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * RAG 向量服务
+ *
+ * @author lingjun.jlj
+ * @date 2025-05-01 17:38
+ */
+@SpringBootApplication
+public class SpringAiVectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiVectorApplication.class, args);
+    }
+
+}
