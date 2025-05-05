f45c5ba152b1dff57b414e9d311fac48cbb0bc9b
tedburner <jlj_vip98@qq.com>
2025-05-05 18:51:52 +0800

    :art: 新增向量化模型

--- spring-boot-canal/src/test/java/com/springboot/canal/SpringBootCanalApplicationTests.java
+++ spring-boot-canal/src/test/java/com/springboot/canal/SpringBootCanalApplicationTests.java
@@ -1,7 +1,7 @@
 package com.springboot.canal;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class SpringBootCanalApplicationTests {
