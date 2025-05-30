3bc4bed3858ae69a2ab408083101cd890f0d16c3
tedburner <jlj_vip98@qq.com>
2025-05-30 17:17:03 +0800

    :art: 新增openmanus框架

---
+++ spring-ai/spring-ai-openmanus/src/test/java/com/ai/openmanus/SpringAiOpenmanusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.openmanus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOpenmanusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
